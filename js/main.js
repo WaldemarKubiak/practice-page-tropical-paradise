c49dbbeaa7762f1cd5e98990afe497b91158493c
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2025-04-05 19:32:34 +0200

    additional animation of nav links

--- js/main.js
+++ js/main.js
@@ -3,12 +3,41 @@ const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
 
 const handleNav = () => {
+	// Toggle klasy aktywujące/usuwa menu
 	nav.classList.toggle('nav__items-container--active');
+
+	// Jeśli menu jest zamykane, usuń animację
+	if (!nav.classList.contains('nav__items-container--active')) {
+		removeNavItemsAnimation(); // Usuwamy animację, jeśli menu jest zamknięte
+	}
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('nav__items-container--active');
+			removeNavItemsAnimation(); // Usuwamy animację po kliknięciu w element menu
 		});
+	});
+
+	// Jeśli menu jest otwarte, dodajemy animację
+	if (nav.classList.contains('nav__items-container--active')) {
+		handleNavItemsAnimation();
+	}
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.add('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+
+const removeNavItemsAnimation = () => {
+	allNavItems.forEach(item => {
+		item.classList.remove('nav-items-animation');
+		item.style.animationDelay = ''; // Resetowanie opóźnienia animacji
 	});
 };
 
